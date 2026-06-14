3a79ba84afa3daef3ed2e1650b2190695874a7e6
hy4962 <hy4962@vip.qq.com>
2026-06-14 14:09:18 +0800

    添加项目文件。

---
+++ WPF_ChatRoom/CustomControl/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_ChatRoom.CustomControl
+{
+    public class CustomButton : Button
+    {
+        /// <summary>
+        /// icon属性
+        /// </summary>
+        public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(string), typeof(CustomButton));
+
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+    }
+}
---
+++ WPF_ChatRoom/ViewModels/MainWindowVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPF_ChatRoom.Views.Pages;
+
+namespace WPF_ChatRoom.ViewModels
+{
+    class MainWindowVM : ViewModelBase
+    {
+        private UserControl _currentPage;
+
+        public UserControl CurrentPage 
+        {
+            get => _currentPage;
+            set { _currentPage = value;OnPropertyChanged(); }
+        }
+        public ICommand HomePage { get; }
+        public ICommand SettingsPage { get; }
+        public ICommand DataBasePage { get; }
+
+        public MainWindowVM()
+        {
+            HomePage = new RelayCommand(_ => CurrentPage = new HomeView());
+            SettingsPage = new RelayCommand(_ => CurrentPage = new Settings());
+            DataBasePage = new RelayCommand(_ => CurrentPage = new DataBaseView());
+
+            // 默认显示首页
+            CurrentPage = new HomeView();
+
+        }
+
+
+    }
+}
